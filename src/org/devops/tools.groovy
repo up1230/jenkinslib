755c138e567db67dd5f37840248869cd696c9b95
up1230 <uphigher@yeah.net>
2025-04-22 16:36:15 +0800

    Update tools.groovy
    use ansicolor plugin print log details

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,6 +1,13 @@
 package org.devops
 
-//打印内容
-def printMes(content){
-  println(content)
+//格式化输出
+def PrintMes(value,color){
+    colors = ['red'    : "\033[40;31m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
+              'green'  : "\033[40;32m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
+              'yellow' : "\033[40;33m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
+              'blue'   : "\033[47;34m >>>>>>>>>>${value}<<<<<<<<<< \033[0m"
+             ]
+    ansiColor('xterm') {
+        println(colors[color])
+    }
 }
